7d78fef8559c73c9a5966554738eadf3d06034c8
kl3ag2 <kl3ag2@komp17.lo1cg.org>
2017-10-12 08:46:10 +0200

    odczyt i wstawianie danych kl3ag2

--- sql_zadanie1/baza_sql.py
+++ sql_zadanie1/baza_sql.py
@@ -4,14 +4,27 @@
 #  baza_sql.py
 
 import sqlite3
-
+from dane import *
 
 def main(args):
     con = sqlite3.connect(':memory:')
-    cur = con.cursor()  # utworzenie kursora
+    cur = con.cursor()
+    
+    with open('pracownicy.sql', 'r') as plik:
+        cur.executescript(plik.read())
+    premia = dane_z_pliku('premia.txt')
+    premia = wyczysc_dane(premia, 1)
+    
+    pracownicy = dane_z_pliku('pracownicy.txt')
+    pracownicy = wyczysc_dane(pracownicy,5)
+    
+    dzial = dane_z_pliku('dział.txt')
+    cur.executemany('INSERT INTO dzial VALUES (?, ?, ?)', dzial)
+    cur.executemany('INSERT INTO premia VALUES (?, ?)', premia)
+    cur.executemany('INSERT INTO pracownicy (id, nazwisko, imie, stanowisko, data_zatr, placa, id_dzial) VALUES (?, ?, ?, ?, ?, ?, ?)', pracownicy)
     
     return 0
-
+    
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
